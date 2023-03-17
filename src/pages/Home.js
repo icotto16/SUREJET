7a868babefb6877e5cd7d9b7cd76baa83a9735bb
REDEXCT <121300580+REDEXCT@users.noreply.github.com>
2023-03-17 00:07:11 -0400

    Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to SUREJET</h2>
+      <p>Your ultimate private jet booking service.</p>
+    </div>
+  );
+};
+
+export default Home;
