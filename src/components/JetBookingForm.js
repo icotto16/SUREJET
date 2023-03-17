0e30056b52f235e9956c3583aee86c1994b2a434
REDEXCT <121300580+REDEXCT@users.noreply.github.com>
2023-03-17 00:41:34 -0400

    Update JetBookingForm.js

--- src/components/JetBookingForm.js
+++ src/components/JetBookingForm.js
@@ -6,6 +6,7 @@ import {
   getSigner,
   toWei,
 } from './contracts';
+import WertWidgetInitializer from '@wert-io/widget-initializer';
 
 const JetBookingForm = ({ jets }) => {
   const [selectedJet, setSelectedJet] = useState(null);
@@ -17,16 +18,30 @@ const JetBookingForm = ({ jets }) => {
     const jet = jets.find((j) => j.id === parseInt(selectedJet));
     if (!jet) return;
 
-    const signer = await getSigner();
-    const wireTransferTokenL2Instance = await getWireTransferTokenL2Instance();
-    const l2GatewayInstance = await getL2GatewayInstance();
+    const wertWidget = WertWidgetInitializer();
+    const paymentData = {
+      amount: jet.price, // Set the amount to the jet price
+      currency: 'USD', // Set the currency
+      // ... any other payment data properties
+    };
 
-    // Approve L2 Gateway to transfer tokens for the booking
-    const approvalAmount = toWei(jet.price);
-    await wireTransferTokenL2Instance.connect(signer).approve(l2GatewayInstance.address, approvalAmount);
+    // Launch the Wert.io payment widget
+    const paymentResult = await wertWidget.launch(paymentData);
 
-    // Initiate the withdrawal for the booking
-    await l2GatewayInstance.connect(signer).initiateWithdrawal(approvalAmount);
+    if (paymentResult && paymentResult.status === 'success') {
+      const signer = await getSigner();
+      const wireTransferTokenL2Instance = await getWireTransferTokenL2Instance();
+      const l2GatewayInstance = await getL2GatewayInstance();
+
+      // Approve L2 Gateway to transfer tokens for the booking
+      const approvalAmount = toWei(jet.price);
+      await wireTransferTokenL2Instance.connect(signer).approve(l2GatewayInstance.address, approvalAmount);
+
+      // Initiate the withdrawal for the booking
+      await l2GatewayInstance.connect(signer).initiateWithdrawal(approvalAmount);
+    } else {
+      console.error('Payment failed');
+    }
   };
 
   return (
@@ -35,4 +50,17 @@ const JetBookingForm = ({ jets }) => {
       <form onSubmit={handleSubmit}>
         <label htmlFor="jet">Select a Jet:</label>
         <select id="jet" onChange={(e) => setSelectedJet(e.target.value)}>
-          <option value="">--Please choose a
+          <option value="">--Please choose a jet--</option>
+          {jets.map((jet) => (
+            <option key={jet.id} value={jet.id}>
+              {jet.name} - ${jet.price}
+            </option>
+          ))}
+        </select>
+        <button type="submit">Book</button>
+      </form>
+    </div>
+  );
+};
+
+export default JetBookingForm;
