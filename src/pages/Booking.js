532fb9a302adf8d04456fb31cdb023b826a94ccb
REDEXCT <121300580+REDEXCT@users.noreply.github.com>
2023-03-17 00:07:48 -0400

    Booking.js

---
+++ src/pages/Booking.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// Import your JetList and JetBookingForm components here
+
+const Booking = () => {
+  return (
+    <div>
+      <h2>Book a Jet</h2>
+      {/* Render JetList and JetBookingForm components here */}
+    </div>
+  );
+};
+
+export default Booking;
