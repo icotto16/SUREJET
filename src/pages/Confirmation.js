a262bfd3a4c445f404827f28aea5a5b952a835c2
REDEXCT <121300580+REDEXCT@users.noreply.github.com>
2023-03-17 00:08:26 -0400

    Confirmation.js

---
+++ src/pages/Confirmation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Confirmation = () => {
+  return (
+    <div>
+      <h2>Booking Confirmation</h2>
+      <p>Thank you for booking with SUREJET. Your booking has been confirmed.</p>
+    </div>
+  );
+};
+
+export default Confirmation;
